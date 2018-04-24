e9145a2074725f6e4c3eab268009554809cb3aad
Orlayhemmy <daminomics@gmail.com>
2018-04-24 17:11:13 +0100

    refactor to es5

--- config/env/development.js
+++ config/env/development.js
@@ -1,21 +1,25 @@
 require('dotenv').config();
 
-const Info = {
+exports.Info = {
   facebook: {
     clientID: process.env.FB_CLIENT_SECRET,
-    clientSecret: process.env.FB_CLIENT_ID
+    clientSecret: process.env.FB_CLIENT_ID,
+    callbackUrl: 'localhost:3000',
   },
   twitter: {
     clientID: process.env.TWITTER_CONSUMER_KEY,
-    clientSecret: process.env.TWITTER_CONSUMER_SECRET
+    clientSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackUrl: 'localhost:3000',
   },
   google: {
     clientID: process.env.GOOGLE_CLIENT_SECRET,
-    clientSecret: process.env.GOOGLE_CLIENT_ID
+    clientSecret: process.env.GOOGLE_CLIENT_ID,
+    callbackUrl: 'localhost:3000',
   },
   github: {
     clientID: process.env.GITHUB_CLIENT_SECRET,
-    clientSecret: process.env.GITHUB_CLIENT_ID
+    clientSecret: process.env.GITHUB_CLIENT_ID,
+    callbackUrl: 'localhost:3000',
   },
 };
-export default Info;
+
